34d5fda14c2fcbb72751cbe15273420229b09024
Sheng Wei Huang <shengweihuang@dyn184-208.wireless-1725.ndsu.NoDak.edu>
2026-03-18 13:45:09 -0500

    Added HousingLocationInfo Interface

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocation } from '../housing-location/housing-location';
+import { HousingLocationinfo } from '../housing-locationinfo';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,16 @@ import { HousingLocation } from '../housing-location/housing-location';
   styleUrl: './home.css',
 })
 export class Home {
-
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocationinfo = {
+  id: 9999,
+  name: 'Test Home',
+  city: 'Test city',
+  state: 'ST',
+  photo: `${this.baseUrl}/example-house.jpg`,
+  availableUnits: 99,
+  wifi: true,
+  laundry: false,
+};
 }
 
---
+++ src/app/housing-locationinfo.ts
@@ -0,0 +1,10 @@
+export interface HousingLocationinfo {
+    id: number;
+    name: string;
+    city: string;
+    state: string;
+    photo: string;
+    availableUnits: number;
+    wifi: boolean;
+    laundry: boolean;
+}
